d1e5574a45e05eacd1266e205606bf5686daa5b9
robertjh17 <43060067+robertjh17@users.noreply.github.com>
2024-06-12 20:06:15 +0200

    nieuwe start

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import { GeistSans } from "geist/font/sans";
+import { Providers } from "./providers/providers";
+import "./globals.css";
+
+const defaultUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "http://localhost:3000";
+
+export const metadata = {
+  metadataBase: new URL(defaultUrl),
+  title: "Next.js and Supabase Starter Kit",
+  description: "The fastest way to build apps with Next.js and Supabase",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={GeistSans.className}>
+      <body className="bg-background text-foreground">
+        <Providers>
+        <main className="min-h-screen flex flex-col items-center">
+          {children}
+        </main>
+        </Providers>
+      </body>
+    </html>
+  );
+}
