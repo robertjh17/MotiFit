7a8b02a625532ac699de89064aebe7f39eb83da2
robertjh17 <43060067+robertjh17@users.noreply.github.com>
2024-06-14 08:58:06 +0200

    themes toegevoegd

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
+      <body className="">
         <Providers>
-        <main className="min-h-screen flex flex-col items-center">
+        <main className="min-h-screen flex ">
           {children}
         </main>
         </Providers>
