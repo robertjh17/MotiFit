429569c9ca5513b9de53843deaa9d71219add54d
robertjh17 <43060067+robertjh17@users.noreply.github.com>
2024-06-15 13:30:23 +0200

    dark mode chakra toegevoegd

--- app/providers/providers.tsx
+++ app/providers/providers.tsx
@@ -1,7 +1,13 @@
 'use client'
 
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider} from '@chakra-ui/react'
+import theme from './theme'
+import { ColorModeScript } from '@chakra-ui/react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>
+  return <><ColorModeScript initialColorMode={theme.config.initialColorMode} />
+  <ChakraProvider theme={theme}>
+    {children}
+  </ChakraProvider>
+  </>
 }
