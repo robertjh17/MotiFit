f7628e769c590cf60eec3d7957828de7ead70cf2
robertjh17 <43060067+robertjh17@users.noreply.github.com>
2024-06-19 15:26:55 +0200

    probeersel

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -69,20 +69,46 @@ export default function Login({
         </Heading>
       </CardHeader>
               <CardBody>
-          <FormControl id="email">
-            <FormLabel>Email address</FormLabel>
-            <Input type="email" />
-          </FormControl>
-          <FormControl id="password" mt={4}>
-            <FormLabel>Password</FormLabel>
-            <Input type="password" />
-          </FormControl>
-          <SubmitButton
-            pendingText="Signing In..."
-            formAction={signIn}
-            >
-              Sign in
-            </SubmitButton>
+              <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
+                <label className="text-md" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                  name="email"
+                  placeholder="you@example.com"
+                  required
+                />
+                <label className="text-md" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="rounded-md px-4 py-2 bg-inherit border mb-6"
+                  type="password"
+                  name="password"
+                  placeholder="••••••••"
+                  required
+                />
+                <SubmitButton
+                  formAction={signIn}
+                  className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+                  pendingText="Signing In..."
+                >
+                  Sign In
+                </SubmitButton>
+                <SubmitButton
+                  formAction={signUp}
+                  className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+                  pendingText="Signing Up..."
+                >
+                  Sign Up
+                </SubmitButton>
+                {searchParams?.message && (
+                  <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
+                    {searchParams.message}
+                  </p>
+                )}
+              </form>
         </CardBody>
       </Card>
       </div>
